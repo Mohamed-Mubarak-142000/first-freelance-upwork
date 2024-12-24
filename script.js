4b31474a5fc4cca32fd94aa9ac950867cc7627b5
mmubarak.innovatek <mmubarak.innovatek@outlook.com>
2024-12-24 16:54:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startWorkButton = document.getElementById("startWorkButton");
+  const popupModal = new bootstrap.Modal(document.getElementById("popupModal"));
+  const images = document.querySelectorAll(".row img");
+
+  // Show initial popup modal
+  startWorkButton.addEventListener("click", () => {
+    popupModal.show();
+  });
+
+  // Add click event to each image
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      const targetModalId = image.getAttribute("data-target");
+      const targetModal = new bootstrap.Modal(
+        document.querySelector(targetModalId)
+      );
+      targetModal.show();
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.getElementById("createButton");
+  createButton.addEventListener("click", () => {
+    const currentModal = bootstrap.Modal.getInstance(
+      document.getElementById("modal1")
+    );
+    currentModal.hide();
+    const targetModalId = createButton.getAttribute("data-target");
+    const targetModal = new bootstrap.Modal(
+      document.querySelector(targetModalId)
+    );
+    targetModal.show();
+  });
+});
