962aa68eacd5e4f658d8b77b3687627d16c52760
mmubarak.innovatek <mmubarak.innovatek@outlook.com>
2024-12-25 19:56:39 +0200

    first commit

--- script.js
+++ script.js
@@ -110,6 +110,65 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.getElementById("createSurveyInfo");
+  createButton.addEventListener("click", () => {
+    const currentModal = bootstrap.Modal.getInstance(
+      document.getElementById("modal3")
+    );
+    currentModal.hide();
+    const targetModalId = createButton.getAttribute("data-target");
+    const targetModal = new bootstrap.Modal(
+      document.querySelector(targetModalId)
+    );
+    targetModal.show();
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.getElementById("sosButton");
+  createButton.addEventListener("click", () => {
+    const currentModal = bootstrap.Modal.getInstance(
+      document.getElementById("modal4")
+    );
+    currentModal.hide();
+    const targetModalId = createButton.getAttribute("data-target");
+    const targetModal = new bootstrap.Modal(
+      document.querySelector(targetModalId)
+    );
+    targetModal.show();
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.getElementById("systemButton");
+  createButton.addEventListener("click", () => {
+    const currentModal = bootstrap.Modal.getInstance(
+      document.getElementById("modal14")
+    );
+    currentModal.hide();
+    const targetModalId = createButton.getAttribute("data-target");
+    const targetModal = new bootstrap.Modal(
+      document.querySelector(targetModalId)
+    );
+    targetModal.show();
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.getElementById("eventButton");
+  createButton.addEventListener("click", () => {
+    const currentModal = bootstrap.Modal.getInstance(
+      document.getElementById("modal5")
+    );
+    currentModal.hide();
+    const targetModalId = createButton.getAttribute("data-target");
+    const targetModal = new bootstrap.Modal(
+      document.querySelector(targetModalId)
+    );
+    targetModal.show();
+  });
+});
 function changeMainImage(src) {
   const mainImage = document.getElementById("mainImage");
   mainImage.src = src;
